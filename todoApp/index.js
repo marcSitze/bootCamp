722743415d02d2d40534a86f51b615ffe5630d43
marcSitze <marcsitze01@gmail.com>
2022-07-02 13:42:53 +0100

    initial commit

---
+++ todoApp/index.js
@@ -0,0 +1,90 @@
+const form = document.querySelector('#form');
+const addBtn = document.querySelector('.addTodo');
+const editBtn = document.querySelector('.editTodo');
+const ul = document.querySelector('ul');
+
+let todos = [];
+let updating = false;
+let todoToUpdate = '';
+// { id, text}
+
+const item = (data) => `<li><span>${data.text}</span><button data-id=${data.id} class="close">X</button></li>`
+
+// const getUpdateTodo = todo => {
+//   console.log('id edit', todo);
+//   form.text.value = todo.text;
+//   console.log('todos: ', todos);
+//   editBtn.style.display = 'block';
+// }
+
+// const updateTodo = () => {
+//   todos.map((i, index) => {
+//     if(String(i.id) === String(todoToUpdate)){
+//       console.log('eqal');
+//       console.log('form.text.value: ', form.text.value)
+//       console.log('index: ', i[index])
+//       todos[index] = {
+//         ...i[index],
+//         text: form.text.value ?? form.text.value
+//       }
+//     }
+
+//     ul.innerHTML = '';
+
+//     todos.forEach(todo => {
+//       ul.innerHTML += item(todo);
+//     })
+//   })
+// }
+
+const addTodo = e => {
+
+  if(form.text.value) {
+    // add todo to our array of todos
+    const data = {
+      id: generateID(),
+      text: form.text.value,
+    };
+
+    todos.push(data);
+    console.log(todos);
+
+    // update the UI
+      ul.innerHTML += item(data);
+      form.reset();
+      form.text.focus();
+  }
+}
+
+const generateID = () => {
+  if(todos.length === 0) {
+    return 1;
+  }else{
+    return todos[todos.length - 1].id + 1
+  }
+}
+
+const deleteTodo = id => {
+    todos = todos.filter((data) => data.id !== Number(id));
+    ul.innerHTML = '';
+
+    todos.forEach(todo => {
+      ul.innerHTML += item(todo);
+    })
+
+}
+
+addBtn.addEventListener('click', addTodo);
+ul.addEventListener('click', e => {
+  window.e = e;
+  const id = e.target.getAttribute('data-id');
+  if(e.target.classList.contains('close') && e.target.getAttribute('data-id')) {
+    deleteTodo(id);
+  }
+  // if(e.target.classList.contains('edit') && e.target.getAttribute('data-id')){
+  //   todoToUpdate = id;
+  //   const todo = todos.find(todo => todo.id === Number(id));
+  //   getUpdateTodo(todo);
+  // }
+});
+// editBtn.addEventListener('click', updateTodo)
